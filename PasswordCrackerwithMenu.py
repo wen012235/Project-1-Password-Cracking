e60a4e5861aad9945f77b6ea864e62412ca181d7
nawe1321 <66645143+nawe1321@users.noreply.github.com>
2020-06-09 11:02:22 -0600

    I commented everything out.

--- PasswordCrackerwithMenu.py
+++ PasswordCrackerwithMenu.py
@@ -1,13 +1,16 @@
 from urllib.request import urlopen, hashlib
 
+#provides a menu with 3 options
 def menulist():
-    print(
+        print(
         "Welcome to the password cracker.  \n Press 1 to enter an already hashed password \n Press 2 if you need to hash your password \n Press 3 for brute force without hashing \n")
     menu = input("How would you like to proceed?\n")
 
+#if you already have an md5 hash, then alreadyhashed (defined below) will run
     if menu == "1":
         alreadyhashed()
 
+#if you only have the plaintext password, this will provide an md5 hash of that password, and then loop you back to run alreadyhashed from the first menu option
     elif menu == "2":
         password = input("Input the password to hash\n>")
         print("\nMD5 hash:\n")
@@ -16,45 +19,51 @@ def menulist():
         guess_pw = hash_object.hexdigest()
         print(guess_pw)
         print("Copy and paste the above hash into the password cracker.\n Are you ready to proceed?")
-        passwordhashmenu = input("press Y to continue or any other key to return to the main menu:\n")
-        if passwordhashmenu == "Y" or passwordhashmenu == "y":
+        passwordhashmenu=input("press Y to continue or any other key to return to the main menu:\n")
+        if passwordhashmenu =="Y" or passwordhashmenu=="y":
             alreadyhashed()
         else:
             menulist()
 
+#this will run a brute force on your password
     elif menu == "3":
-        print("We're still working on that")
+        bruteforce()
 
+#if you provide an option other than 1, 2, or 3
     else:
         print("Invalid entry.  Try again")
         menulist()
 
 def alreadyhashed():
-    # First, get the hash from the user to get the md5 hash to crack
+    # the user inputs the md5 hash to crack
     md5hash = input("Please input the hash to crack.\n>")
 
-    # Second, we'll open a file full of password guesses
+    # this provides a password list of the top 10,000 passwords
     LIST_OF_COMMON_PASSWORDS = str(urlopen(
         'https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt').read(),
-                                   'utf-8')
+                               'utf-8')
 
-    # Third, we'll take a guess from the list of passwords we opened, and split it by line
+    # a for loop to look line by line at each of those 10,000 passwords
     for guess in LIST_OF_COMMON_PASSWORDS.split('\n'):
 
-        # Fourth, we'll hash the guess we took from the password list so we can compare it to the hash the user gave us
+        # a condensed version of what we did in menu option 2 above, where we hash the common passwords from the list
         hashedGuess = hashlib.md5(bytes(guess, 'utf-8')).hexdigest()
 
-        # Fifth, we'll compare the hash the user gave us to the hashed version of the password guess and determine if they are equal
+        # comparing each of the hashed common passwords to the user input to determine if there is a match
         if hashedGuess == md5hash:
 
-            # Sixth, we'll tell the program what to do if the password guess matches, which is to print the current guess and quit the program.
-            # We'll also tell the program what to do if the password guess don't match, which is to return to step 3 to get a new password from the list
+            # if they match, print the correct guess and quit the program.
             print("The password is", str(guess))
             quit()
+        # or else, if they don't match, continue to the next hashed password and compare those
         elif hashedGuess != md5hash:
             print("Password guess ", str(guess), " does not match, trying next...")
 
-    # In the seventh and final step, we'll tell the program what to do if we get all the way through the password list without finding a match.
+    # if the password list is exhausted and no match is found
     print("Password not in database, we'll get them next time.")
 
+def bruteforce():
+    print("We're still working on that")
+
+#initiates the program
 menulist()
