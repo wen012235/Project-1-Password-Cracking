2310f7f7a217aa23c6c63a24339e1418062f4136
Preston Church <pchurch2@uccs.edu>
2020-06-10 21:21:10 -0600

    Moved hash compare code to its own function to be used for both Dictionary and Brute Force methods.

--- PasswordCrackerwithMenu.py
+++ PasswordCrackerwithMenu.py
@@ -1,17 +1,24 @@
 from urllib.request import urlopen, hashlib
 import time
 
+
+#global variables
+attempts = 0
+start = 0
+end = 0
+
+
 #provides a menu with 3 options
 def menulist():
     print(
         "Welcome to the password cracker.  \n Press 1 to enter an already hashed password \n Press 2 if you need to hash your password \n Press 3 for brute force without hashing \n")
     menu = input("How would you like to proceed?\n")
 
-#if you already have an md5 hash, then alreadyhashed (defined below) will run
+    #if you already have an md5 hash, then alreadyhashed (defined below) will run
     if menu == "1":
         alreadyhashed()
 
-#if you only have the plaintext password, this will provide an md5 hash of that password, and then loop you back to run alreadyhashed from the first menu option
+    #if you only have the plaintext password, this will provide an md5 hash of that password, and then loop you back to run alreadyhashed from the first menu option
     elif menu == "2":
         password = input("Input the password to hash\n>")
         print("\nMD5 hash:\n")
@@ -21,21 +28,26 @@ def menulist():
         print(guess_pw)
         print("Copy and paste the above hash into the password cracker.\n Are you ready to proceed?")
         passwordhashmenu=input("press Y to continue or any other key to return to the main menu:\n")
+        
         if passwordhashmenu =="Y" or passwordhashmenu=="y":
             alreadyhashed()
         else:
             menulist()
 
-#this will run a brute force on your password
+    #this will run a brute force on your password
     elif menu == "3":
         bruteforce()
 
-#if you provide an option other than 1, 2, or 3
+    #if you provide an option other than 1, 2, or 3
     else:
         print("Invalid entry.  Try again")
         menulist()
 
+
 def alreadyhashed():
+    #reference global variable start
+    global start
+    
     # the user inputs the md5 hash to crack
     md5hash = input("Please input the hash to crack.\n>")
 
@@ -45,34 +57,43 @@ def alreadyhashed():
                                'utf-8')
 
     start = time.time()
-    attempts = 0
+
     # a for loop to look line by line at each of those 10,000 passwords
     for guess in LIST_OF_COMMON_PASSWORDS.split('\n'):
-
-        # a condensed version of what we did in menu option 2 above, where we hash the common passwords from the list
-        hashedGuess = hashlib.md5(bytes(guess, 'utf-8')).hexdigest()
-
-        # comparing each of the hashed common passwords to the user input to determine if there is a match
-        if hashedGuess == md5hash:
-            end = time.time()
-            # if they match, print the correct guess and quit the program.
-            print("The password is", str(guess))
-            if attempts==1:
-                print("It took", str(end-start), "seconds\nand", str(attempts), "attempt to crack your password")
-            else:
-                print("It took", str(end - start), "seconds\nand", str(attempts), "attempts to crack your password")
-            quit()
-
-        # or else, if they don't match, continue to the next hashed password and compare those
-        elif hashedGuess != md5hash:
-            print("Password guess", str(guess), " does not match, trying next...")
-            attempts+=1
+        hashcompare(md5hash, guess)
 
     # if the password list is exhausted and no match is found
-    print("Password not in database, we'll get them next time.")
+    print("\nPassword not in database, we'll get them next time.")
+
 
 def bruteforce():
     print("We're still working on that")
+
+
+def hashcompare(md5hash_compare, guess_compare):
+    # reference global variables end and attempts
+    global end
+    global attempts
+
+    # a condensed version of what we did in menu option 2 above, where we hash the common passwords from the list
+    hashedGuess = hashlib.md5(bytes(guess_compare, 'utf-8')).hexdigest()
+
+    # comparing each of the hashed common passwords to the user input to determine if there is a match
+    if hashedGuess == md5hash_compare:
+        end = time.time()
+        # if they match, print the correct guess and quit the program.
+        print("\nThe password is", str(guess_compare))
+        if attempts==1:
+            print("It took", str(end-start), "seconds\nand", str(attempts), "attempt to crack your password")
+        else:
+            print("It took", str(end - start), "seconds\nand", str(attempts), "attempts to crack your password")
+        quit()
+
+    # or else, if they don't match, continue to the next hashed password and compare those
+    elif hashedGuess != md5hash_compare:
+        print("Password guess", str(guess_compare), " does not match, trying next...")
+        attempts+=1
+
 
 #initiates the program
 menulist()
